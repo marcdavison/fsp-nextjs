67a9db48f50989148594ebcdb8b184f7e3de0f50
MARC DAVISON <marcdavison@MARCs-MBP.lan>
2025-10-24 00:45:12 +0100

    copy in and update basic session cookie check, client calls api with value, session stores cookie and redirects, middleware checks

---
+++ app/auth/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function ClientComponent() {
+  const router = useRouter();
+
+
+  function sendCorrect() {
+    const value = 'correct-value';
+    fetch('/api/session', {
+      method: 'POST',
+      body: JSON.stringify({ value }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log('cookie stored .. ');
+          router.push('/dashboard'); // ✅ This triggers a GET request
+        } else {
+          console.error('Failed to store cookie');
+        }
+      })
+      .catch((err) => {
+        console.error('Error posting to API:', err);
+      });
+
+
+  }
+
+  function sendIncorrect() {
+    const value = 'incorrect-value';
+    fetch('/api/session', {
+      method: 'POST',
+      body: JSON.stringify({ value }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log('cookie stored .. ');
+          router.push('/dashboard'); // ✅ This triggers a GET request
+        } else {
+          console.error('Failed to store cookie');
+        }
+      })
+      .catch((err) => {
+        console.error('Error posting to API:', err);
+      });
+
+  }
+
+  return <div>
+    <p>Click a value to send to the api</p>
+    <p><input type="button" onClick={sendCorrect} value="Correct" /></p>
+    <p><input type="button" onClick={sendIncorrect}  value="Incorrect" /></p>
+  </div>;
+}
---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const sessionValue = request.cookies.get('sessionValue')?.value;
+  console.log('✅ Middleware engaged');
+  console.log('🧠 Session cookie value in middleware:', sessionValue);
+  if (sessionValue !== 'correct-value') {
+    console.log('🚫 Invalid or missing session cookie. Redirecting to /other');
+    return NextResponse.redirect(new URL('/other', request.url));
+  }
+
+  console.log('✅ Middleware good');
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard'], // Only intercept requests to /dashboard
+};
