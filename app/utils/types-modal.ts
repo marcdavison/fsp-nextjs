e2c1ec93b136be54ed5a83ada082fd0ffab8e01b
MARC DAVISON <marcdavison@MARCs-MBP.lan>
2025-10-23 18:17:45 +0100

    add homepage along with required css, install view-transitions for page nav, add utils and assets from public folder

---
+++ app/utils/types-modal.ts
@@ -0,0 +1,10 @@
+export enum ModalType {
+  EDIT_NAME = 'edit-name',
+  PREDICTION_SAVED = 'predictions-saved',
+  INVALID_PREDICTIONS = 'invalid-predictions',
+  WILDCARD_CREATE = 'wildcards-create',
+  JOINED_PRIVATE_LEAGUE = 'joined-private-league',
+  FORGOTTEN_PASSWORD = 'forgotten-password',
+  NEW_ACCOUNT = 'new-account',
+  USER_NOT_VERIFIED = 'user-not-verified',
+}
